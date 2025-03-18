1a5db8cc5534d11d2ceec273acc4e443df11a4b2
Natarajan-Rayi <natarajan.rayi@gmail.com>
2025-03-18 08:56:14 +0530

    souce code committed

---
+++ src/content/Management/Users/userForm.tsx
@@ -0,0 +1,130 @@
+import SidebarLayout from '@/layouts/SidebarLayout';
+import { useState, useEffect } from 'react';
+import Box from '@mui/material/Box';
+import {
+  Grid,
+  Card,
+  CardHeader,
+  CardContent,
+  Divider,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Dialog,
+  TextField,
+  Button
+} from '@mui/material';
+
+const UserModals = ({ open, handleClose, editUser }) => {
+  const [role, setRole] = useState('');
+  const [gender, setGender] = useState('');
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    mobileNumber: '',
+    designation: '',
+    employeeID: '',
+    address: ''
+  });
+
+  // Populate form fields when editing a user
+  useEffect(() => {
+    if (editUser) {
+      setUserData({
+        name: editUser.name || '',
+        email: editUser.email || '',
+        mobileNumber: editUser.mobileNumber || '',
+        designation: editUser.designation || '',
+        employeeID: editUser.employeeID || '',
+        address: editUser.address || ''
+      });
+      setRole(editUser.role || '');
+      setGender(editUser.gender || '');
+    } else {
+      // Reset form for creating a new user
+      setUserData({ name: '', email: '', mobileNumber: '', designation: '', employeeID: '', address: '' });
+      setRole('');
+      setGender('');
+    }
+  }, [editUser]);
+
+  return (
+    <>
+      <Dialog onClose={handleClose} open={open}>
+        <Grid container direction="row" justifyContent="center" alignItems="stretch" spacing={3}>
+          <Grid item xs={12}>
+            <Card>
+              <CardHeader title={editUser ? 'Edit User' : 'Add User'} />
+              <Divider />
+              <CardContent>
+                <Box component="form" noValidate autoComplete="off">
+                  <Grid container spacing={2}>
+                    {/* User Role */}
+                    <Grid item xs={12}>
+                      <FormControl fullWidth>
+                        <InputLabel>User Role</InputLabel>
+                        <Select value={role} onChange={(e) => setRole(e.target.value)}>
+                          <MenuItem value="Admin">Admin</MenuItem>
+                          <MenuItem value="User">User</MenuItem>
+                          <MenuItem value="Manager">Manager</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+
+                    {/* Two Fields Per Row */}
+                    <Grid item xs={6}>
+                      <TextField label="Name" type="text" fullWidth value={userData.name} onChange={(e) => setUserData({ ...userData, name: e.target.value })} />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField label="Email" type="text" fullWidth value={userData.email} onChange={(e) => setUserData({ ...userData, email: e.target.value })} />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <TextField label="Mobile Number" type="text" fullWidth value={userData.mobileNumber} onChange={(e) => setUserData({ ...userData, mobileNumber: e.target.value })} />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField label="Designation" type="text" fullWidth value={userData.designation} onChange={(e) => setUserData({ ...userData, designation: e.target.value })} />
+                    </Grid>
+
+                    <Grid item xs={6}>
+                      <TextField label="Emp ID" type="text" fullWidth value={userData.employeeID} onChange={(e) => setUserData({ ...userData, employeeID: e.target.value })} />
+                    </Grid>
+                    
+                    {/* Gender */}
+                    <Grid item xs={6}>
+                      <FormControl fullWidth>
+                        <InputLabel>Gender</InputLabel>
+                        <Select value={gender} onChange={(e) => setGender(e.target.value)}>
+                          <MenuItem value="Male">Male</MenuItem>
+                          <MenuItem value="Female">Female</MenuItem>
+                          <MenuItem value="Other">Other</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </Grid>
+
+                    {/* Address */}
+                    <Grid item xs={12}>
+                      <TextField label="Address" type="text" multiline rows={4} fullWidth value={userData.address} onChange={(e) => setUserData({ ...userData, address: e.target.value })} />
+                    </Grid>
+
+                    {/* Submit Button */}
+                    <Grid item xs={12} display="flex" justifyContent="flex-end">
+                      <Button variant="contained" color="primary">
+                        {editUser ? 'Update' : 'Create'}
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Dialog>
+    </>
+  );
+};
+
+UserModals.getLayout = (page) => <SidebarLayout>{page}</SidebarLayout>;
+
+export default UserModals;
