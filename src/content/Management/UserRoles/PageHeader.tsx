6e0b4eb0aa813404e83dff1c17643221fa8f13c9
Durkesnandhini N <durkesnandhini@gmail.com>
2025-03-18 21:20:56 +0530

    role

--- src/content/Management/UserRoles/PageHeader.tsx
+++ src/content/Management/UserRoles/PageHeader.tsx
@@ -1,11 +1,13 @@
 import { Typography, Button, Grid } from '@mui/material';
 
 import AddTwoToneIcon from '@mui/icons-material/AddTwoTone';
+import { useState } from 'react';
+import UserRoleCreateModals from "./CreateRole";
 
 function PageHeader() {
-  const user = {
-    name: 'Catherine Pike',
-    avatar: '/static/images/avatars/1.jpg'
+  const [modalOpen, setMadalOpen] = useState(false);
+  const handleModalClose = () => {
+    setMadalOpen(false);
   };
   return (
     <Grid container justifyContent="space-between" alignItems="center">
@@ -13,18 +15,17 @@ function PageHeader() {
         <Typography variant="h3" component="h3" gutterBottom>
           User Management
         </Typography>
-        <Typography variant="subtitle2">
-          {user.name}, these are your recent transactions
-        </Typography>
       </Grid>
       <Grid item>
         <Button
           sx={{ mt: { xs: 2, md: 0 } }}
           variant="contained"
           startIcon={<AddTwoToneIcon fontSize="small" />}
+          onClick={()=>setMadalOpen(true)}
         >
           Create Roles
         </Button>
+        <UserRoleCreateModals open={modalOpen} handleModalClose={handleModalClose}/>
       </Grid>
     </Grid>
   );
