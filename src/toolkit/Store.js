1395950828a3b0d21f419396f431d3955393c192
Durkesnandhini N <durkesnandhini@gmail.com>
2025-03-25 22:20:32 +0530

    delete file

--- src/toolkit/Store.js
+++
@@ -1,9 +0,0 @@
-// src/toolkit/store.js
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers/rootReducer";
-
-const store = configureStore({
-  reducer: rootReducer,
-});
-
-export default store;
